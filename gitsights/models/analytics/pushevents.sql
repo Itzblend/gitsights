17d4ff4c328c234bdb73bad1edbbc42c01f743e7
Itzblend <huhta.lauri@gmail.com>
2021-05-03 22:35:35 +0300

    WIP dbt and jenkins

---
+++ gitsights/models/analytics/pushevents.sql
@@ -0,0 +1,30 @@
+{{ config(materialized='table') }}
+
+-- pushevents view
+ SELECT id,
+        type,
+        actorid,
+        actorlogin,
+        repoid,
+        reponame,
+        payloadpushid,
+        payloadsize,
+        payloadhead,
+        payloadbefore,
+        public,
+        created_at,
+        (organization ->> 'id')::INT             AS organization_id,
+        organization ->> 'login'                 AS organization_name,
+        -- payloadcommit
+        (payloadcommit ->> 'push_id')::BIGINT    AS push_id,
+        (payloadcommit ->> 'size')::INT          AS size,
+        (payloadcommit ->> 'distinct_size')::INT AS distinct_size,
+        commit ->> 'sha'                         AS sha,
+        fromjson(payloadcommit.commit.author.email)           AS author_email,
+        commit #>> '{{author, name}}'            AS author_name,
+        commit ->> 'message'                     AS message,
+        commit ->> 'distinct'                    AS distinct,
+        commit ->> 'url'                         AS commit_url
+ FROM git_t.events_t,
+      json_array_elements(payloadcommit -> 'commits') AS commit
+WHERE type = 'PushEvent'
